79284bb1ae2f180a4cd2df8c9ff56ff7d0367d65
David Vinicius <david.silva.ext@infracommerce.com.br>
2022-02-11 15:18:06 -0300

    created method to check if text is morse

--- src/morse.js
+++ src/morse.js
@@ -38,10 +38,49 @@ var _map = {
     ' ': '  '
 };
 
+const _mapMorseToLetter = {
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
+    ' ': ''
+}
+
 const morse = {
     encode: (text) => {
         let encodedText = "";
-        let append = "";
         for (let char of text.toLowerCase()) {
             if (char == " ")
                 encodedText += _map[char]
@@ -51,8 +90,30 @@ const morse = {
 
         return encodedText.substring(0, encodedText.length - 1)
     },
-    decode: () => {
+    decode: (text) => {
+        let morse_words = text.split(/\s\s|\s\s\s|\//g);
 
+        let textDecoded = "";
+        for (let word of morse_words) {
+            let chars = word.split(" ");
+            for (let char of chars) {
+                textDecoded += _mapMorseToLetter[char]
+            }
+            textDecoded += " ";
+        }
+        return textDecoded.substring(0, textDecoded.length - 1);
+    },
+    isMorseCode: (text) => {
+        let morse_words = text.split(/\s\s|\s\s\s|\//g);
+
+        for (let word of morse_words) {
+            let chars = word.split(" ");
+            let isMorse = chars.every((char) => char in _mapMorseToLetter);
+            if (isMorse === false) return false;
+
+        }
+
+        return true;
     }
 }
 
--- tests/morse.test.js
+++ tests/morse.test.js
@@ -8,3 +8,13 @@ test('test encode word "ABC"', () => {
     expect(morse.encode("ABC ABC")).toBe(".- -... -.-.   .- -... -.-.");
     expect(morse.encode("A time A")).toBe(".-   - .. -- .   .-");
 });
+
+
+test('decode encoded word "ABC"', () => {
+    expect(morse.decode(".- -... -.-.")).toBe("abc");
+});
+
+test('discover if text is morse code', () => {
+    expect(morse.isMorseCode(".- -... -.-.")).toBe(true);
+    expect(morse.isMorseCode("teste")).toBe(false);
+});
